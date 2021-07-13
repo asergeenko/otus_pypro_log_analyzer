bda70522ac6a6306cfb234f8f292e97fff56819f
Aleksandr Sergeenko <asergeenko@gmail.com>
2021-07-13 18:23:02 +0300

    HW second update

--- log_analyzer.py
+++ log_analyzer.py
@@ -11,7 +11,7 @@ import json
 from datetime import date,datetime
 
 from string import Template
-from collections import OrderedDict
+from collections import OrderedDict,namedtuple
 
 import argparse
 
@@ -54,6 +54,9 @@ config = {
            "LOG_LEVEL": "DEBUG"
 }
 
+LatestLog = namedtuple('LatestLog', 'path ext max_date')
+RequestData = namedtuple('RequestData', 'urls time_total count_total')
+
 
 def find_latest_log(config):
     """
@@ -61,7 +64,7 @@ def find_latest_log(config):
 
     :param dict config: Log analyzer configuration
     :return: Path to log file, log file exstension, latest log date
-    :rtype: tuple
+    :rtype: namedtuple of (path, ext, max_date)
     """
     max_date = date.min
     logpath = ''
@@ -76,36 +79,35 @@ def find_latest_log(config):
 
         try:
             cur_date = datetime.strptime(match.group('request_date'),'%Y%m%d').date()
-            if cur_date > max_date:
-                max_date = cur_date
-                logpath = filename
-                logext = ext
         except ValueError as e:
             logging.error("Incorrect date: %s"%(match.group('request_date')))
+            continue
+
+        if cur_date > max_date:
+            max_date = cur_date
+            logpath = filename
+            logext = ext
 
     if not logpath or logext not in ['', '.gz']:
         logging.info("No log file detected")
     elif logpath:
         logging.info("Found log: '%s'" % (logpath,))
 
-    return logpath, logext, max_date
+    return LatestLog(logpath, logext, max_date)
 
 
 
 
-def parse(config,logfile,logext):
+def parse(config,latest_log):
     """
     Iterate log file and yield parsed url and request_time
 
     :param config: Log analyzer configuration
-    :param logfile: Path to log file
-    :param logext: Log file exstension
-    :return: url, request_time
+    :param latest_log: LatestLog namedtuple
+    :return: None if got a parse error, (url, request_time) otherwise
     """
-    openfunc = gzip.open if logext == '.gz' else open
-    with openfunc(os.path.join(config['LOG_DIR'], logfile), "rt", encoding="utf-8") as fp:
-        num_errors = 0
-        num_lines = 0
+    openfunc = gzip.open if latest_log.ext == '.gz' else open
+    with openfunc(os.path.join(config['LOG_DIR'], latest_log.path), "rt", encoding="utf-8") as fp:
         for line in fp:
             match = re.search(LOG_LINE_REGEX, line)
             if match:
@@ -113,51 +115,46 @@ def parse(config,logfile,logext):
                 request_time = float(match.group('request_time'))
                 yield url, request_time
             else:
-                num_errors += 1
-            num_lines += 1
-        if num_errors / num_lines > config["MAX_ERROR_RATE"]:
-            logging.error("Maximum error rate reached in '%s'" % (logfile,))
+                # Parse error
+                yield
 
-
-def read_config(default_config):
+def read_config(config_path, default_config):
     """
-    Parse command line, read config file if exists and merge with the default config
+    Read config file if exists and merge with the default config
 
-    :param config: Default log analyzer configuration
-    :return: Log analyzer configuration
+    :param config_path: Path to config file
+    :param default_config: Default log analyzer configuration
+    :return: Log analyzer configuration if config file exists None otherwise
     """
 
-    args = parser.parse_args()
-
-    config_path = os.path.join(CONFIG_DIR,args.config_path if args.config_path else 'config.json')
     try:
         with open(config_path, "r", encoding="utf-8") as fp:
             # Merge configs (config from file has a priority)
             return {**config, **json.load(fp)}
     except IOError as e:
-        return default_config
+        logging.error("Could not read config file '%s'. %s" % (config_path, e))
 
 def read_template(template_path):
     '''
+    Reads template into an str
 
     :param template_path: Path to template file
-    :return: Template str
+    :return: Template str if file exists, None otherwise
     '''
     try:
         with open(config['TEMPLATE'], 'r', encoding='utf-8') as fp:
             return fp.read()
     except IOError as e:
-        logging.error("Could not read template file: '%s'. %s" % (config['TEMPLATE'], str(e)))
-        return ''
+        logging.error("Could not read template file: '%s'. %s" % (config['TEMPLATE'], e))
 
-def collect_request_data(config, logpath, logext):
+def collect_request_data(config, latest_log, parse_func):
     '''
     Collects parsed request data (number of requests, urls, request time)
 
     :param config: config dict
-    :param logpath: path to parsed log file
-    :param logext: log file extension
-    :return: urls: dict of urls, time_total: total request time, count_total: total number of requests
+    :param latest_log: LatestLog namedtuple
+    :param parse_func: log parser
+    :return: RequestData (dict of urls, total request time, total number of requests) if parsing is ok, None otherwise
     '''
     urls = {}
     # Total request number
@@ -165,7 +162,19 @@ def collect_request_data(config, logpath, logext):
     # Total request time
     time_total = 0
 
-    for url, request_time in parse(config, logpath, logext):
+    num_errors = 0
+    num_lines = 0
+
+    for row in parse_func(config, latest_log):
+
+        num_lines += 1
+
+        if not row:
+            num_errors += 1
+            continue
+
+        url, request_time = row
+
         time_total += request_time
         count_total += 1
         if url in urls:
@@ -175,31 +184,33 @@ def collect_request_data(config, logpath, logext):
         else:
             urls[url] = {'count': 1, 'time': [request_time], 'time_sum': request_time}
 
+    if num_errors / num_lines > config["MAX_ERROR_RATE"]:
+        logging.error("Maximum error rate reached in '%s'" % (latest_log.path,))
+        return
+
     urls = OrderedDict(sorted(urls.items(),
                               key=lambda key_value: key_value[1]['time_sum'],
                               reverse=True)[:config['REPORT_SIZE']])
 
-    return urls, time_total, count_total
+    return RequestData(urls, time_total, count_total)
 
-def calc_stats(urls,time_total,count_total):
+def calc_stats(request_data):
     '''
-
-    :param urls: OrderedDict
-    :param time_total: Total request time
-    :param count_total: Total number of requests
+    :param request_data: namedtuple of (urls, time_total, count_total)
     :return: JS-ready statistics for the HTML template
     '''
+
     table_json = []
-    for url, value in urls.items():
+    for url, value in request_data.urls.items():
         sorted_time = sorted(value['time'])
         len_time = len(sorted_time)
         half_len_time = len_time // 2
         table_json.append({
             'url': url,
             'count': value['count'],
-            'count_perc': round(value['count'] / count_total, 6),
+            'count_perc': round(value['count'] / request_data.count_total, 6),
             'time_sum': round(value['time_sum'], 4),
-            'time_perc': round(value['time_sum'] / time_total, 6),
+            'time_perc': round(value['time_sum'] / request_data.time_total, 6),
             'time_avg': round(value['time_sum'] / len_time, 4),
             'time_max': round(sorted_time[-1], 4),
             'time_med': round(sorted_time[half_len_time], 4) if len_time % 2 else \
@@ -207,61 +218,68 @@ def calc_stats(urls,time_total,count_total):
         })
     return table_json
 
-def create_report(reportdir, reportpath, templ, table_json):
+def create_report(reportdir, reportpath, templatepath, table_json):
     '''
     Renders template and writes a HTML report file
 
     :param reportdir: report directory
     :param reportpath: report path
-    :param templ: template string
+    :param templatepath: template path
     :param table_json: JSON stats table
     '''
+
+    try:
+        with open(templatepath, 'r', encoding='utf-8') as fp:
+            template = fp.read()
+    except IOError as e:
+        logging.error("Could not read template file: '%s'. %s" % (templatepath, e))
+        return
 
     try:
         if not os.path.isdir(reportdir):
             os.mkdir(reportdir)
 
         with open(reportpath,'w',encoding='utf-8') as fout:
-            fout.write(Template(templ).safe_substitute(table_json=json.dumps(table_json)))
+            fout.write(Template(template).safe_substitute(table_json=json.dumps(table_json)))
             logging.info("Report succesfully created: '%s'" % (reportpath))
     except IOError as e:
-        logging.error("Could not write file: '%s'\n%s"%(reportpath,str(e)))
+        logging.error("Could not write file: '%s'\n%s"%(reportpath, e))
 
 def main(default_config):
 
-    # Read config file
-    config = read_config(default_config)
+    args = parser.parse_args()
+    config_path = os.path.join(CONFIG_DIR, args.config_path if args.config_path else 'config.json')
+
+    config = read_config(config_path, default_config)
     if not config:
         return
 
-    logging.basicConfig(filename=config.get('LOG_FILE', None), level=config.get('LOG_LEVEL','DEBUG'),
+    logging.basicConfig(filename=config.get('LOG_FILE'), level=config.get('LOG_LEVEL','DEBUG'),
                         format='[%(asctime)s] %(levelname).1s %(message)s', datefmt='%Y.%m.%d %H:%M:%S')
 
-
-    templ = read_template(config['TEMPLATE'])
-    if not templ:
-        return
-
-    logpath, logext, max_date = find_latest_log(config)
-    if not logpath:
+    latest_log = find_latest_log(config)
+    if not latest_log.path:
         # Log file not found
         return
 
     # Check if report is already created
-    reportpath = os.path.join(config['REPORT_DIR'], max_date.strftime(REPORT_FILE_TEMPLATE))
+    reportpath = os.path.join(config['REPORT_DIR'], latest_log.max_date.strftime(REPORT_FILE_TEMPLATE))
     if os.path.isfile(reportpath):
-        logging.info("Report for '%s' already created" % (logpath,))
+        logging.info("Report for '%s' already created" % (latest_log.path,))
         return
 
-    urls, time_total, count_total = collect_request_data(config, logpath, logext)
+    request_data = collect_request_data(config, latest_log, parse)
 
-    table_json = calc_stats(urls, time_total, count_total)
+    if not request_data:
+        return
 
-    create_report(config['REPORT_DIR'], reportpath, templ, table_json)
+    table_json = calc_stats(request_data)
+
+    create_report(config['REPORT_DIR'], reportpath, config['TEMPLATE'], table_json)
 
 
 if __name__ == "__main__":
     try:
         main(config)
     except BaseException as e:
-        logging.exception(str(e))
+        logging.exception(e)
--- test_log_analyzer.py
+++ test_log_analyzer.py
@@ -3,6 +3,7 @@ import log_analyzer as la
 import sys
 import os
 import logging
+import datetime
 
 CONFIG_SAMPLE = {
     "REPORT_SIZE": 2000,
@@ -25,53 +26,44 @@ class LogAnalyzerTest(unittest.TestCase):
 
     def test_good_config_file(self):
         sys.argv+=['--config','config.json']
-        self.assertEqual(la.read_config(la.config), CONFIG_SAMPLE)
+        self.assertEqual(la.read_config('./config/config.json',la.config), CONFIG_SAMPLE)
 
 
     def test_no_log(self):
-        config = la.read_config(la.config)
+        config = la.read_config('./config/config.json', la.config)
         config['LOG_DIR'] = './test/empty'
         self.assertEqual(la.find_latest_log(config)[0],'')
 
     def test_bz2_log(self):
-        config = la.read_config(la.config)
+        config = la.read_config('./config/config.json', la.config)
         config['LOG_DIR'] = './test/bz2'
         self.assertEqual(la.find_latest_log(config)[0], '')
 
 
     def test_plain_log(self):
-        config = la.read_config(la.config)
+        config = la.read_config('./config/config.json', la.config)
         config['LOG_DIR'] = './test/plain'
-        self.assertEqual(la.find_latest_log(config), ('nginx-access-ui.log-20200831', '', os.path.join(config['REPORT_DIR'],'report-2020.08.31.html')))
+        self.assertEqual(la.find_latest_log(config), ('nginx-access-ui.log-20200831', '', datetime.date(2020,8,31)))
 
 
     def test_gz_log(self):
-        config = la.read_config(la.config)
+        config = la.read_config('./config/config.json', la.config)
         config['LOG_DIR'] = './test/gz'
-        self.assertEqual(la.find_latest_log(config), ('nginx-access-ui.log-20200831.gz', '.gz', os.path.join(config['REPORT_DIR'],'report-2020.08.31.html')))
+        self.assertEqual(la.find_latest_log(config),
+                         la.LatestLog('nginx-access-ui.log-20200831.gz', '.gz',
+                                      datetime.date(2020,8,31)))
 
     def test_bad_log(self):
-        config = la.read_config(la.config)
+        config = la.read_config('./config/config.json', la.config)
         config['LOG_DIR'] = './test/bad'
         logfile = 'nginx-access-ui.log-20200831'
         logging.basicConfig(filename=config.get('LOG_FILE', None), level=logging.DEBUG,
                             format='[%(asctime)s] %(levelname).1s %(message)s', datefmt='%Y.%m.%d %H:%M:%S')
-        for line in la.parse(config, logfile, ''):
-            pass
+
+        request_data = la.collect_request_data(config, la.LatestLog(logfile,'',datetime.date(2020,8,31)), la.parse)
+
         with open(config['LOG_FILE'],"r") as fp:
             self.assertIn("Maximum error rate reached in '%s'" % (logfile,), fp.readlines()[-1])
-
-    def test_already_parsed(self):
-        config = la.read_config(la.config)
-        config['LOG_DIR'] = './test/plain'
-        config['REPORT_DIR'] = './test/reports'
-
-        logging.basicConfig(filename=config.get('LOG_FILE', None), level=logging.DEBUG,
-                            format='[%(asctime)s] %(levelname).1s %(message)s', datefmt='%Y.%m.%d %H:%M:%S')
-        la.find_latest_log(config)
-
-        with open(config['LOG_FILE'],"r") as fp:
-            self.assertIn("Report for '%s' already created" % ('nginx-access-ui.log-20200831',), fp.readlines()[-1])
 
 
 if __name__ == '__main__':
